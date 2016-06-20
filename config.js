dff0872ab50a526d762ede289092ea6e150a54ca
shayk89 <shaykemp89@gmail.com>
2016-06-20 13:22:26 -0700

    Update config.js
    update for Afterglow

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'Deep In My Bones',
+  community: process.env.COMMUNITY_NAME || 'Afterglow',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'bonesrp.slack.com',
+  slackUrl: process.env.SLACK_URL || 'xafterglowx.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-33385380535-33387327863-33499487510-0cef33134b',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-52570391763-52619448661-52612809958-7cafefd080',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
